d7e4cb87c9a674febaad774d4bd54f1577a3c3a2
nhat minh <minhtn.22ns@vku.udn.vn>
2026-05-28 10:22:47 +0700

    Them ma doc de demo tinh nang chan luong CI/CD

--- demo-app/server.js
+++ demo-app/server.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
   res.send('Ứng dụng này chứa rất nhiều lỗi bảo mật!');
 });
 
+// Lập trình viên vô tình để quên Token ở đây (Trivy sẽ bắt được lỗi này)
+const GITHUB_TOKEN = 'ghp_1234567890abcdef1234567890abcdef1234';
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
